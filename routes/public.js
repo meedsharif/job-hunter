d4381e6281e774f094771865724e6e58fd4f5651
taawraat <50162704+taawraat@users.noreply.github.com>
2020-03-27 22:06:38 +0600

    Update public.js

--- routes/public.js
+++ routes/public.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
   res.render('index');
 })
 
+router.get('/investors', (req, res) => {
+  res.render('investors');
+});
+
+router.get('/employee', (req, res) => {
+  res.render('employee');
+});
+
+router.get('/partner', (req, res) => {
+  res.render('partner');
+});
+
 module.exports = router;
